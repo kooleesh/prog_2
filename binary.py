53a1e0195a527e91529246ae11fe6cba7fc38d51
kooleesh <lerakulish2@gmail.com>
2025-10-08 16:37:18 +0300

    Add files via upload

---
+++ binary.py
@@ -0,0 +1,17 @@
+def gen_bin_tree(height: int, root: int) -> dict:
+    if height < 1:
+        return {}
+    if height == 1:
+        return {str(root): {}}  # Лист без детей
+    return {
+        str(root): {
+            "left leaf": gen_bin_tree(height - 1, root * 3),
+            "right leaf": gen_bin_tree(height - 1, root - 4)
+        }
+    }
+
+# tree = (gen_bin_tree(4, 7))
+# import pprint
+# pprint.pprint(tree)
+
+print(gen_bin_tree(5, 3))
